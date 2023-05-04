c5c47fd68f2057dbcb9ec167fbb9fcc03ec57967
bw02184 <bw02184@naver.com>
2023-05-04 20:58:35 +0900

    페이징 전 ~159p

---
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -0,0 +1,42 @@
+package org.zerock.guestbook.service;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.zerock.guestbook.dto.GuestbookDTO;
+
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class GuestbookServiceImpl implements GuestbookService {
+
+    private final GuestbookRepository repository;
+
+    @Override
+    public Long register(GuestbookDTO dto) {
+
+        log.info("DTO------------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
+        repository.save(entity);
+
+        return entity.getGno();
+    }
+
+}
